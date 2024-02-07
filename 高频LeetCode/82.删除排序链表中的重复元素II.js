db7dc36ae293c76c78d8315b2a90c9434cb8f805
H <94888499+w040150594@users.noreply.github.com>
2024-02-07 17:30:47 +0800

    feat:  leeatcode 82 83 删除排序链表中的重复元素

---
+++ 高频LeetCode/82.删除排序链表中的重复元素II.js
@@ -0,0 +1,61 @@
+// 描述：给定一个排序链表，删除所有含有重复数字的结点，只保留原始链表中 没有重复出现的数字。
+// 示例 1:
+// 输入: 1->2->3->3->4->4->5
+// 输出: 1->2->5
+// 示例 2:
+// 输入: 1->1->1->2->3
+// 输出: 2->3
+
+// 创建dummy结点就是咱们人为制造出来的第一个结点的前驱结点
+function ListNode(val) {
+  this.val = val
+  this.next = null
+}
+
+const deleteDuplicates = head => {
+  if (!head || !head.next) return head
+  let dummy = new ListNode()
+  dummy.next = head
+
+  let cur = dummy
+  // cur后面至少有两个节点
+  while (cur.next && cur.next.next) {
+    if (cur.next.val === cur.next.next.val) {
+      // 保存相同值
+      const val = cur.next.val
+      // 遍历检查之后还有没有相同的值,有则删除
+      while (cur.next && cur.next.val === val) {
+        cur.next = cur.next.next
+      }
+    } else {
+      cur = cur.next
+    }
+  }
+
+  return dummy.next
+}
+// 时间复杂度 O(n), 空间复杂度 O(1)
+// 测试
+const head = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 3,
+        next: {
+          val: 4,
+          next: {
+            val: 4,
+            next: {
+              val: 5,
+              next: null
+            }
+          }
+        }
+      }
+    }
+  }
+}
+console.log(deleteDuplicates(head)) // 1 -> 2 -> 5
---
+++ 高频LeetCode/83.删除排序链表中的重复元素.js
@@ -0,0 +1,31 @@
+// 真题描述：给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+// 示例 1:
+// 输入: 1->1->2
+// 输出: 1->2
+// 示例 2:
+// 输入: 1->1->2->3->3
+// 输出: 1->2->3
+
+// 解题思路：遍历链表，如果当前节点的值和下一个节点的值相等，则删除下一个节点，否则继续遍历。
+const deleteDuplicates = head => {
+  let cur = head
+
+  while (cur !== null && cur.next !== null) {
+    if (cur.val === cur.next.val) cur.next = cur.next.next
+    else cur = cur.next
+  }
+  return head
+}
+
+const head = {
+  val: 1,
+  next: {
+    val: 1,
+    next: {
+      val: 2,
+      next: null
+    }
+  }
+}
+
+console.log(deleteDuplicates(head)) // 1 -> 2
